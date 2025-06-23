95964ad386edeab61b8497fd08ceb3f6b66d9c48
DEUS-DEI <16824459+DEUS-DEI@users.noreply.github.com>
2025-06-23 09:38:48 +0000

    Reestructurar el script de construcción del addon para eliminar el directorio de construcción y optimizar la exclusión de archivos innecesarios. Actualizar el archivo addon.xml para mejorar la estructura y la legibilidad.

--- build_addon.bat
+++ build_addon.bat
@@ -2,29 +2,16 @@
 echo Building MAL Tracker Addon for Kodi...
 
 set ADDON_NAME=plugin.video.maltracker
-set BUILD_DIR=build
 set ZIP_NAME=%ADDON_NAME%-1.0.0.zip
 
-if exist %BUILD_DIR% rmdir /s /q %BUILD_DIR%
-mkdir %BUILD_DIR%
-mkdir %BUILD_DIR%\%ADDON_NAME%
-
-echo Copying addon files...
-xcopy /s /e /q %ADDON_NAME%\*.* %BUILD_DIR%\%ADDON_NAME%\
-
 echo Excluding unnecessary files...
-del /q %BUILD_DIR%\%ADDON_NAME%\*.pyc 2>nul
-del /q %BUILD_DIR%\%ADDON_NAME%\resources\*.pyc 2>nul
-rmdir /s /q %BUILD_DIR%\%ADDON_NAME%\__pycache__ 2>nul
-rmdir /s /q %BUILD_DIR%\%ADDON_NAME%\resources\__pycache__ 2>nul
+if exist %ADDON_NAME%\*.pyc del /q %ADDON_NAME%\*.pyc
+if exist %ADDON_NAME%\resources\*.pyc del /q %ADDON_NAME%\resources\*.pyc
+if exist %ADDON_NAME%\__pycache__ rmdir /s /q %ADDON_NAME%\__pycache__
+if exist %ADDON_NAME%\resources\__pycache__ rmdir /s /q %ADDON_NAME%\resources\__pycache__
 
 echo Creating ZIP package...
-cd %BUILD_DIR%
-powershell -command "Compress-Archive -Path '%ADDON_NAME%' -DestinationPath '../%ZIP_NAME%' -Force"
-cd ..
-
-echo Cleaning up...
-rmdir /s /q %BUILD_DIR%
+powershell -command "Compress-Archive -Path '%ADDON_NAME%' -DestinationPath '%ZIP_NAME%' -Force"
 
 echo.
 echo Addon package created: %ZIP_NAME%
